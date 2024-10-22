bd248f3e5e2697b74bb26d8d703dfc5af64155d8
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-10-22 09:39:34 +0100

    add current player and winning combinations variables

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 const cells = document.querySelectorAll('.cell');
 const messageEl = document.querySelector('.message');
+const players = ['X', '0'];
+let currentPlayer = players[0];
+const winningCombinations = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
