2f69cfc0ca6fd17fa6e640f50e0688f1e1b51cd3
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-10-23 15:53:56 +0100

    populate word holder letters on correct guess and remove a life when incorrect

--- hangman/index.js
+++ hangman/index.js
@@ -1,8 +1,10 @@
-const keyboardList = document.querySelector('.list');
-
+let NumberOfGuesses = 9;
 const testWord = 'development';
-
 const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+
+const keyboardList = document.querySelector('.list');
+const livesRemaining = document.querySelector('.lives-remaining');
+const wordHolder = document.querySelector('.word-holder');
 
 for (const letter of alphabet) {
   const list = document.createElement('li');
@@ -15,13 +17,41 @@ for (const letter of alphabet) {
 
 const allKeys = document.querySelectorAll('.key');
 
+livesRemaining.textContent = NumberOfGuesses;
+wordHolder.textContent = testWord
+  .split('')
+  .map(() => '_')
+  .join(' ');
+
+function checkIfGuessIsCorrect(guess, word) {
+  console.log(guess, word);
+  return word.includes(guess);
+}
+
+function handleGuess(guess, word) {
+  if (checkIfGuessIsCorrect(guess, word)) {
+    const updatedWord = word
+      .split('')
+      .map((letter, index) => {
+        if (letter === guess) {
+          return letter;
+        } else {
+          return wordHolder.textContent.split(' ')[index];
+        }
+      })
+      .join(' ');
+    wordHolder.textContent = updatedWord;
+  } else {
+    // TODO: Disable guessed key and draw a part of the hangman
+    NumberOfGuesses--;
+    livesRemaining.textContent = NumberOfGuesses;
+  }
+}
+
 for (const key of allKeys) {
   key.addEventListener('click', () => {
-    console.log(key.textContent);
-    if (testWord.includes(key.textContent)) {
-      console.log('correct');
-    } else {
-      console.log('incorrect');
-    }
+    handleGuess(key.textContent, testWord);
+    key.disabled = true;
+    key.style.cursor = 'not-allowed';
   });
 }
