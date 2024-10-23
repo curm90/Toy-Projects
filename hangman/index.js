191aadacee8f29e46ab712df91fffcd5ea030fb9
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-10-23 14:29:22 +0100

    log correct values if clicked letter is in the answer word

--- hangman/index.js
+++ hangman/index.js
@@ -1,13 +1,27 @@
-const keyboard = document.querySelector('.keyboard');
 const keyboardList = document.querySelector('.list');
+
+const testWord = 'development';
 
 const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
 
 for (const letter of alphabet) {
-  const li = document.createElement('li');
+  const list = document.createElement('li');
   const button = document.createElement('button');
   button.classList.add('key');
   button.textContent = letter;
-  li.appendChild(button);
-  keyboardList.appendChild(li);
+  list.appendChild(button);
+  keyboardList.appendChild(list);
+}
+
+const allKeys = document.querySelectorAll('.key');
+
+for (const key of allKeys) {
+  key.addEventListener('click', () => {
+    console.log(key.textContent);
+    if (testWord.includes(key.textContent)) {
+      console.log('correct');
+    } else {
+      console.log('incorrect');
+    }
+  });
 }
