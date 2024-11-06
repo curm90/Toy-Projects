a3ef9d3d615863cb71f4c575dced75298f9be376
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-11-06 17:26:32 +0000

    randomise symbols and add them to each card on the back.

--- memory-cards/script.js
+++ memory-cards/script.js
@@ -1,7 +1,55 @@
+const symbols = [
+  '🍕',
+  '🍔',
+  '🍟',
+  '🌭',
+  '🍿',
+  '🍦',
+  '🍩',
+  '🍪',
+  '🍕',
+  '🍔',
+  '🍟',
+  '🌭',
+  '🍿',
+  '🍦',
+  '🍩',
+  '🍪',
+];
+const matchesToWin = symbols.length / 2;
+
 const cardContainers = document.querySelectorAll('.card-container');
+const frontFaces = document.querySelectorAll('.back');
+// const turnsCounter = document.querySelector('.turns-counter');
+
+function checkIfMatch(el1, el2) {
+  return el1.textContent === el2.textContent;
+}
 
 cardContainers.forEach((cardContainer) => {
   cardContainer.addEventListener('click', () => {
     cardContainer.classList.toggle('flipped');
   });
 });
+
+function shuffle(array) {
+  let currentIndex = 0;
+
+  while (currentIndex < array.length - 1) {
+    const randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex++;
+
+    [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+  }
+
+  return array;
+}
+
+const randomSymbols = shuffle(symbols);
+
+randomSymbols.forEach((symbol, i) => {
+  console.log({ symbol });
+  frontFaces[i].textContent = symbol;
+});
+
+console.log({ frontFaces });
