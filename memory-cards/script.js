18abf69c4d2f9feabcba2f1271a4600662e31d58
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-11-07 12:28:50 +0000

    persist matched cards when found, and flip back pairs of unmatched cards

--- memory-cards/script.js
+++ memory-cards/script.js
@@ -17,18 +17,57 @@ const symbols = [
   '🍪',
 ];
 const matchesToWin = symbols.length / 2;
+let flippedCards = [];
 
 const cardContainers = document.querySelectorAll('.card-container');
 const frontFaces = document.querySelectorAll('.back');
 // const turnsCounter = document.querySelector('.turns-counter');
 
 function checkIfMatch(el1, el2) {
-  return el1.textContent === el2.textContent;
+  return el1 === el2;
+}
+
+function getSymbolUniCode(symbol) {
+  return symbol.codePointAt(0);
+}
+
+function flipUnmatchCardsBack(card1, card2) {
+  setTimeout(() => {
+    card1.classList.remove('flipped');
+    card2.classList.remove('flipped');
+  }, 1000);
 }
 
 cardContainers.forEach((cardContainer) => {
   cardContainer.addEventListener('click', () => {
     cardContainer.classList.toggle('flipped');
+
+    flippedCards.push(cardContainer);
+
+    console.log({ flippedCards });
+
+    if (flippedCards.length === 2) {
+      const [firstCard, secondCard] = flippedCards;
+
+      const firstCardTextContent = firstCard.querySelector('.back').textContent;
+      const secondCardTextContent = secondCard.querySelector('.back').textContent;
+
+      console.log({ firstCardTextContent, secondCardTextContent });
+      const symbolOneCode = getSymbolUniCode(firstCardTextContent);
+      const symbolTwoCode = getSymbolUniCode(secondCardTextContent);
+
+      console.log({ symbolOneCode, symbolTwoCode });
+
+      if (checkIfMatch(firstCardTextContent, secondCardTextContent)) {
+        console.log('Match!');
+        firstCard.removeEventListener('click', () => {});
+        secondCard.removeEventListener('click', () => {});
+        flippedCards = [];
+      } else {
+        flipUnmatchCardsBack(firstCard, secondCard);
+        flippedCards = [];
+      }
+    }
   });
 });
 
