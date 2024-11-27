76068e7b02e1a1a091b48512ec71a5e580b8ca05
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-11-27 11:59:54 +0000

    add words to find in the grid

--- word-search/script.js
+++ word-search/script.js
@@ -1,20 +1,60 @@
+const wordsToFind = [
+  'code',
+  'programming',
+  'developer',
+  'software',
+  'engineer',
+  'javascript',
+  'react',
+  'host',
+  'server',
+  'web',
+];
+
 const wordSearch = document.querySelector('.word-search');
+const selectedWord = document.querySelector('.selected-word');
+const wordsToFindList = document.querySelector('.words-list');
 
 function generateRandomLetter() {
   const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
   return alphabet[Math.floor(Math.random() * alphabet.length)];
 }
 
-function generateWordSearchGrid(size) {
-  const grid = [];
+function findRandomStartPointForWord(word, gridSize) {
+  const mazStartPoint = gridSize - word.length;
+  const row = Math.floor(Math.random() * gridSize);
+  const col = Math.floor(Math.random() * (mazStartPoint + 1));
 
-  for (let i = 0; i < size; i++) {
-    const row = [];
-    for (let j = 0; j < size; j++) {
-      row.push(generateRandomLetter());
-    }
-    grid.push(row);
+  return { row, col };
+}
+
+function placeWordHorizontally(grid, word, startPoint) {
+  const { row, col } = startPoint;
+
+  for (let i = 0; i < word.length; i++) {
+    grid[row][col + i] = word[i];
   }
+}
+
+function fillEmptyCells(grid) {
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[i].length; j++) {
+      if (grid[i][j] === null) {
+        grid[i][j] = generateRandomLetter();
+      }
+    }
+  }
+}
+
+function generateWordSearchGrid(size) {
+  const grid = Array.from({ length: size }, () => Array.from({ length: size }).fill(null));
+
+  wordsToFind.forEach((word) => {
+    const startPoint = findRandomStartPointForWord(word, size);
+    placeWordHorizontally(grid, word, startPoint);
+  });
+
+  fillEmptyCells(grid);
 
   return grid;
 }
@@ -36,7 +76,16 @@ function renderWordSearchGrid(grid) {
   });
 }
 
+function renderWordsToFind(words) {
+  words.forEach((word) => {
+    const wordItem = document.createElement('li');
+    wordItem.textContent = word;
+    wordsToFindList.appendChild(wordItem);
+  });
+}
+
 document.addEventListener('DOMContentLoaded', () => {
   const grid = generateWordSearchGrid(12);
   renderWordSearchGrid(grid);
+  renderWordsToFind(wordsToFind);
 });
