75dbbc6b4370aeb153ba030daf25cb9361ff2c4e
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2025-01-06 15:06:33 +0000

    add constants file

---
+++ word-search/constants.js
@@ -0,0 +1,13 @@
+export const ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+export const WORDSTOFIND = [
+  'code',
+  // 'programming',
+  // 'developer',
+  // 'software',
+  // 'engineer',
+  // 'javascript',
+  // 'react',
+  'host',
+  'server',
+  'web',
+];
