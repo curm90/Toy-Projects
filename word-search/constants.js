5496ff2af2fcfb9013cc6c7fe375223cfb5f8cd7
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2025-01-09 12:34:15 +0000

    remove hardcoded words to find array

--- word-search/constants.js
+++ word-search/constants.js
@@ -1,13 +1,13 @@
 export const ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-export const WORDSTOFIND = [
-  'code',
-  'programming',
-  'developer',
-  'software',
-  'engineer',
-  'javascript',
-  'react',
-  'host',
-  'server',
-  'web',
-];
+// export const WORDSTOFIND = [
+//   'code',
+//   'programming',
+//   'developer',
+//   'software',
+//   'engineer',
+//   'javascript',
+//   'react',
+//   'host',
+//   'server',
+//   'web',
+// ];
